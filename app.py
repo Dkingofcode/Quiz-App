59d81b40ceb71dde1418acea94b837f23f32960b
Dkingofcode <doladepo128@gmail.com>
2023-01-17 13:33:22 +0000

    Added list of steps in quotes

---
+++ app.py
@@ -0,0 +1,6 @@
+# a dictionary that stores questions and answers
+# have a variable that tracks the score of the player
+# loop through the dictionary using the key value pairs
+# display each question to the user and allow them to answer
+# tell them if they are right or wrong
+# show the final result when quiz is completed 
